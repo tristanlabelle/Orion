28c538379ec87459292f33216b1b6c6f09557e33
Félix Cloutier <>
2009-09-24 20:59:20 +0000

    Added the Rectangle graphical object

---
+++ Production/Sources/Orion.Graphics/Drawing/Rectangle.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using OpenTK;
+using OpenTK.Math;
+using OpenTK.Graphics;
+
+namespace Orion.Graphics.Drawing
+{
+    class Rectangle : IDrawable
+    {
+        public readonly Rect Zone;
+        public readonly System.Drawing.Color Color;
+
+        public Rectangle(Rect rect, System.Drawing.Color color)
+        {
+            Zone = rect;
+            Color = color;
+        }
+
+        public void Fill()
+        {
+            GL.Begin(BeginMode.Polygon);
+            Render();
+            GL.End();
+        }
+
+        public void Stroke()
+        {
+            GL.Begin(BeginMode.Lines);
+            Render();
+            GL.End();
+        }
+
+        private void Render()
+        {
+            Vector2 coords = Zone.Position;
+            GL.Vertex2(coords);
+            coords.Y += Zone.Size.Y;
+            GL.Vertex2(coords);
+            coords.X += Zone.Size.X;
+            GL.Vertex2(coords);
+            coords.Y -= Zone.Size.Y;
+            GL.Vertex2(coords);
+        }
+    }
+}

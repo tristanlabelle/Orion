279f13b2ecd1594b434419ad6ae08bc12e531d40
Aladdin <>
2009-10-07 18:23:14 +0000

    Updated the MapGenerator.

--- Production/Sources/Orion.Core/MapGenerator.cs
+++ Production/Sources/Orion.Core/MapGenerator.cs
@@ -8,23 +8,39 @@ namespace Orion
     public sealed class MapGenerator
     {
         #region Fields
+        
+        double MapLength;
+        double MapHeight;
+        PerlinNoise noise;
 
         #endregion
 
         #region Constructors
 
-        public MapGenerator(int NumberOfPlayers)
+        public MapGenerator(double MapLength, double MapHeight, Random random)
         {
-            
+            this.MapHeight = MapHeight;
+            this.MapLength = MapLength;
+            this.noise = new PerlinNoise(random);
         }
         
         #endregion
 
         #region Properties
 
+        /*public bool Walkable
+        {
+            get {  }
+        }*/
+
         #endregion
 
         #region Methods
+
+        public void Generate()
+        {
+        
+        }
 
         #endregion
     }

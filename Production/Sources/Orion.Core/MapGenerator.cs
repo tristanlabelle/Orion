4c7ee8b6db5b9c6826842ed6a0cb1c9a72dce2ec
Aladdin <>
2009-10-07 17:55:17 +0000

    Added the MapGenerator which doesn't do much right now...

---
+++ Production/Sources/Orion.Core/MapGenerator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Orion
+{
+    public sealed class MapGenerator
+    {
+        #region Fields
+
+        #endregion
+
+        #region Constructors
+
+        public MapGenerator(int NumberOfPlayers)
+        {
+            
+        }
+        
+        #endregion
+
+        #region Properties
+
+        #endregion
+
+        #region Methods
+
+        #endregion
+    }
+}

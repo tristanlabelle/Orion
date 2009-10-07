377fcde5892ca91f7ef69124832e2650d932f705
Mathieu Lavoie <>
2009-10-07 18:19:50 +0000

    First Draft of PathFinding

---
+++ Production/Sources/Orion.GameLogic/PathFinder.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenTK.Math;
+using System.Drawing;
+
+namespace Orion.GameLogic
+{
+    class PathFinder
+    {
+        #region Fields
+        Path theFinalPath;
+        Point source;
+        Point destination;
+        List<PathNode> openNode = new List<PathNode>();
+        List<PathNode> closeNode = new List<PathNode>();
+        #endregion
+
+        #region constructor
+        /// <summary>
+        /// Create a New Object PathFinder that return an object Path.
+        /// </summary>
+        /// <param name="source">The Position of the unit</param>
+        /// <param name="destination">The destination point</param>
+        public PathFinder(Point source, Point destination)
+        {
+            this.source = source;
+            this.destination = destination;
+            theFinalPath = new Path();
+        }
+        private float CalculateMoveCost(PathNode aNode)
+        {
+            return Math.Abs(aNode.Position.X - destination.X) + Math.Abs(aNode.Position.Y - destination.Y);
+        }
+        private void getNearPointToAdd(PathNode currentNode)
+        {
+            for (int j = -1; j <= 1; j++) // colonne 
+                for (int i = -1; i <= 1; i++) // ligne
+                {
+                    if (closeNode.Any(node =>
+                        node.Position.X == (currentNode.Position.X - j) &&
+                        node.Position.Y == (currentNode.Position.X - i))) ;
+                    //TO BE CONTIUNE - MATHIEU
+
+                }
+        }
+        public Path FindPath()
+        {
+            
+            return null;
+        }
+        
+        #endregion
+    }
+}
---
+++ Production/Sources/Orion.GameLogic/PathNode.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenTK.Math;
+using System.Drawing;
+
+namespace Orion.GameLogic
+{
+    public class PathNode
+    {
+        #region Fields
+        PathNode parentNode;
+        Point position;
+
+        
+        float totalCost;
+
+        #endregion
+        #region constructor
+        /// <summary>
+        /// Constructor for a Node (path of terrain).
+        /// </summary>
+        /// <param name="parentNode">The parent represent the fastest way to move from an near by Node </param>
+        /// <param name="position">The x y position of the node in the world</param>
+        /// <param name="totalCost">The cost of the move by adding the deplacement done and the distance from de destination</param>
+        public PathNode(PathNode parentNode, Point position, float totalCost)
+        {
+            this.parentNode = parentNode;
+            this.position = position;
+            this.totalCost = totalCost;
+        }
+        #endregion
+
+        #region Proprieties
+
+        public float TotalCost
+        {
+            get { return totalCost; }
+            set { totalCost = value; }
+        }
+
+        public Point Position
+        {
+            get { return position; }
+            set { position = value; }
+        }
+
+        #endregion
+    }
+}

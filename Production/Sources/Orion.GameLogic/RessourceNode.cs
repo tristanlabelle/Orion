0f13d24cd1f925112269c48440cafd7b7d323a9d
Anthony Vallée-Dubois <>
2009-10-07 16:47:15 +0000

    Implemented the RessourceNode class.

---
+++ Production/Sources/Orion.GameLogic/RessourceNode.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using OpenTK.Math;
+
+namespace Orion.GameLogic
+{
+    class RessourceNode
+    {
+        #region Fields
+
+        private readonly int id;
+        private readonly string ressourceType;
+        private readonly int totalRessources;
+        private int ressourcesLeft;
+        private readonly Vector2 position;
+        private readonly World world;
+
+        #endregion
+
+        #region Constructors
+        public RessourceNode(int id, string ressourceType, int amountOfRessources, Vector2 position, World world)
+        {
+            this.id = id;
+            this.ressourceType = ressourceType;
+            this.totalRessources = amountOfRessources;
+            this.ressourcesLeft = amountOfRessources;
+            this.position = position;
+            this.world = world;
+        }
+        #endregion
+
+        #region Properties
+        public string RessourceType
+        {
+            get { return ressourceType; }
+        }
+
+        public int TotalRessources
+        {
+            get { return totalRessources; }
+        }
+
+        public int RessourcesLeft
+        {
+            get { return ressourcesLeft; }
+            set { ressourcesLeft = value; }
+        }
+
+        public Vector2 Position
+        {
+            get { return position; }
+        }
+
+        #endregion
+
+        #region Methods
+
+        public void Harvest(int amount)
+        {
+            ressourcesLeft -= amount;
+        }
+
+        #endregion
+    }
+}

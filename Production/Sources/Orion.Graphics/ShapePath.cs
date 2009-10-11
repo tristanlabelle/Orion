7b6e9717c4691d9fe82c28e3873b2362eae908cc
Aladdin <>
2009-10-11 00:59:32 +0000

    Added ShapePath and updated some stuff.

---
+++ Production/Sources/Orion.Graphics/ShapePath.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenTK.Math;
+
+namespace Orion.Graphics
+{
+    class ShapePath
+    {
+        #region Fields
+
+        public List<Vector2> Points;
+
+        #endregion
+
+        #region Constructors
+
+        public ShapePath()
+        {
+            Points = new List<Vector2>();
+        }
+
+        #endregion
+
+        #region Properties
+
+        #endregion
+
+        #region Methods
+
+        public void AddPoint(Vector2 Point)
+        {
+            Points.Add(Point);
+        }
+
+        #endregion
+    }
+}

2dd1f21f99458401401f483a63761b0f73771605
Félix Cloutier <>
2009-10-11 18:53:23 +0000

    Greatly simplified the networking code next to a suggestion from Tristan. No longer requires several disparate classes.

--- Production/Sources/Orion.Networking/Transporter.cs
+++ Production/Sources/Orion.Networking/Transporter.cs
@@ -1,322 +1,333 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Net;
 using System.Net.Sockets;
+using System.Diagnostics;
 using System.Threading;
+
+using Orion.Core;
 
 namespace Orion.Networking
 {
-	/// <summary>
-	/// A Transporter is responsible for safely transporting UDP packets over a network. It has the same guaratees the UDP protocol provides, plus
-	/// it guarantees packets are going to be received.
-	/// </summary>
-    public sealed class Transporter : IDisposable
-    {
-        #region Fields
-        #region Private
-
-        private object sessionIdLocker = new object();
-		private const uint localSessionMask = 0x80000000;
-        private static uint nextSessionId;
-
-		private readonly Queue<ReceivingTransaction> completedTransactions = new Queue<ReceivingTransaction>();
-        private readonly Socket udpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
-        private readonly Dictionary<IPEndPoint, Queue<TimeSpan>> answerTimes = new Dictionary<IPEndPoint, Queue<TimeSpan>>();
-        private readonly Dictionary<IPEndPoint, Dictionary<uint, Transaction>> transactions = new Dictionary<IPEndPoint, Dictionary<uint, Transaction>>();
-        private readonly Thread senderThread;
-        private readonly Thread receiverThread;
-        private bool isDisposed;
-
-        #endregion
-        
-        #region Public
-
-		/// <summary>
-		/// Holds the port to which the UDP socket is bound.
-		/// </summary>
-        public readonly int Port;
-
-        #endregion
-        #endregion
-
-        #region Events
-
-		/// <summary>
-		/// The event triggered when a packet is successfully received.
-		/// </summary>
-        public event GenericEventHandler<Transporter, NetworkEventArgs> Received;
-
-        #endregion
-
-        #region Constructor
-
-		/// <summary>
-		/// Creates a Transporter, binding its UDP socket to the specified port.
-		/// </summary>
-		/// <param name="port">
-		/// The port on which to bind
-		/// </param>
-        public Transporter(int port)
-        {
-            Port = port;
-            udpSocket.Bind(new IPEndPoint(IPAddress.Any, port));
+	public class Transporter
+	{
+		#region Nested Types
+		
+		private struct PacketId
+		{
+			public readonly IPEndPoint RemoteHost;
+			public readonly uint SessionId;
+			
+			public PacketId(uint sid, IPEndPoint host)
+			{
+				RemoteHost = host;
+				SessionId = sid;
+			}
+		}
+		
+		private struct PacketData
+		{
+			public readonly PacketId Id;
+			public readonly byte[] Data;
+			
+			public PacketData(PacketId id, byte[] data)
+			{
+				Id = id;
+				Data = data;
+			}
+		}
+		
+		private class PacketSession
+		{
+			private static readonly TimeSpan expiration = new TimeSpan(0, 0, 5);
+			
+			private readonly DateTime creationTime;
+			private Stopwatch timeToReceive;
+			private DateTime whenToResend;
+			private byte[] fullPacket;
+			
+			public readonly PacketId Id;
+			public readonly byte[] Data;
+			
+			public PacketSession(PacketId id, byte[] data)
+			{
+				timeToReceive = new Stopwatch();
+				creationTime = DateTime.UtcNow;
+				whenToResend = creationTime;
+				Id = id;
+				Data = data;
+				
+				fullPacket = new byte[data.Length + 5];
+				fullPacket[0] = DataPacket;
+				BitConverter.GetBytes(id.SessionId).CopyTo(fullPacket, 1);
+				Data.CopyTo(fullPacket, 5);
+			}
+			
+			public void SendThrough(Socket udpSocket)
+			{
+				timeToReceive.Reset();
+				udpSocket.SendTo(fullPacket, Id.RemoteHost);
+			}
+			
+			public void ResetSendTime(long milliseconds)
+			{
+				whenToResend = DateTime.UtcNow + new TimeSpan(0, 0, 0, 0, (int)milliseconds);
+			}
+			
+			public bool NeedsResend
+			{
+				get { return DateTime.UtcNow >= whenToResend; }
+			}
+			
+			public bool HasTimedOut
+			{
+				get { return whenToResend - creationTime > expiration; }
+			}
+			
+			public long Acknowledge()
+			{
+				timeToReceive.Stop();
+				return timeToReceive.ElapsedMilliseconds;
+			}
+		}
+		
+		#endregion
+		
+		#region Fields
+		#region Private
+		private const byte DataPacket = 0;
+		private const byte AcknowledgePacket = 1;
+		private const long DefaultPing = 100;
+		
+		private uint nextSessionId;
+		
+		private readonly Queue<NetworkEventArgs> readyData = new Queue<NetworkEventArgs>();
+		
+		private readonly Dictionary<IPEndPoint, Queue<long>> pings = new Dictionary<IPEndPoint, Queue<long>>();
+		private readonly List<PacketId> answeredPackets = new List<PacketId>();
+		private readonly List<PacketId> packetsToAnswer = new List<PacketId>();
+		
+		private readonly Dictionary<PacketId, PacketSession> packetsToSend = new Dictionary<PacketId, PacketSession>();
+		private readonly Socket udpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+		
+		private readonly Thread senderThread;
+		private readonly Thread receiverThread;
+		private bool isDisposed;
+		
+		#endregion
+		
+		#region Public
+		public readonly int Port;
+		#endregion
+		#endregion
+		
+		#region Events
+		
+		public event GenericEventHandler<Transporter, NetworkEventArgs> Received;
+		
+		#endregion
+		
+		#region Constructor
+		public Transporter(int port)
+		{
+			Port = port;
+			udpSocket.Bind(new IPEndPoint(IPAddress.Any, port));
+			udpSocket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReceiveTimeout, 5000);
 			senderThread = new Thread(SenderThread);
 			receiverThread = new Thread(ReceiverThread);
-			
-			senderThread.Start();
-			receiverThread.Start();
-        }
-
-        #endregion
-
-        #region Methods
-
-        #region Private
-
-        private void CheckIfDisposed()
-        {
-            if (isDisposed)
-            {
-                throw new ObjectDisposedException(null);
-            }
-        }
-
-        private void SenderThread()
-        {
-            while (true)
-            {
-                if (isDisposed) break;
-
-                lock (transactions)
-                {
-                    foreach(KeyValuePair<IPEndPoint, Dictionary<uint, Transaction>> hostPair in transactions)
-                    {
-                        IPEndPoint remoteHost = hostPair.Key;
-						lock(hostPair.Value)
-						{
-	                        foreach (KeyValuePair<uint, Transaction> pair in hostPair.Value)
-	                        {
-	                            Transaction transaction = pair.Value;
-								lock(transaction)
-								{
-		                            if (transaction.IsReady)
-		                            {
-		                                uint sessionId = pair.Key ^ localSessionMask;
+		}
+		#endregion
 		
-		                                byte[] dataToSend = transaction.Send();
-		                                byte[] packetData = new byte[sizeof(int) + dataToSend.Length];
+		#region Methods
 		
-		                                BitConverter.GetBytes(sessionId).CopyTo(packetData, 0);
-		                                dataToSend.CopyTo(packetData, sizeof(int));
-		                                udpSocket.SendTo(packetData, remoteHost);
-		                            }
-								}
-	                        }
-						}
-                    }
-                }
-
-                Thread.Sleep(10);
-            }
-        }
-
-        private void ReceiverThread()
-        {
-            byte[] buffer = new byte[512];
-            EndPoint endpointFrom = null;
-            while (true)
-            {
-                if (isDisposed) break;
-
-                udpSocket.ReceiveFrom(buffer, ref endpointFrom);
-                IPEndPoint from = endpointFrom as IPEndPoint;
-				
-				Dictionary<uint, Transaction> hostTransactions;
-				lock(transactions)
+		private void ValidateObjectState()
+		{
+			if(isDisposed) throw new ObjectDisposedException(null);
+		}
+		
+		private void AddPing(IPEndPoint host, long milliseconds)
+		{
+			Queue<long> hostPings;
+			lock(pings)
+			{
+				if(!pings.ContainsKey(host))
 				{
-					if(!transactions.ContainsKey(from))
+					pings[host] = new Queue<long>();
+				}
+				hostPings = pings[host];
+			}
+			
+			lock(hostPings)
+			{
+				hostPings.Enqueue(milliseconds);
+				if(hostPings.Count > 50)
+				{
+					hostPings.Dequeue();
+				}
+			}
+		}
+		
+		private long AveragePing(IPEndPoint host)
+		{
+			Queue<long> hostPings;
+			lock(pings)
+			{
+				if(!pings.ContainsKey(host))
+				{
+					pings[host] = new Queue<long>();
+				}
+				hostPings = pings[host];
+			}
+			
+			lock(hostPings)
+			{
+				if(hostPings.Count == 0)
+					return DefaultPing;
+				return (long)hostPings.Average();
+			}
+		}
+		
+		private long StandardDeviationForPings(IPEndPoint host)
+		{
+			long average = AveragePing(host);
+			
+			long deviation = 0;
+			Queue<long> hostPings;
+			lock(pings)
+			{
+				hostPings = pings[host];
+			}
+			
+			lock(hostPings)
+			{
+				foreach(long ping in hostPings)
+				{
+					long pingDeviation = average - ping;
+					deviation += Math.Sqrt(pingDeviation * pingDeviation);
+				}
+				deviation /= hostPings.Count();
+			}
+			
+			return deviation;
+		}
+		
+		private void ReceiverThread()
+		{
+			byte[] answer = new byte[5];
+			answer[0] = AcknowledgePacket;
+			
+			byte[] packet = new byte[1024];
+			EndPoint endpoint = new IPEndPoint(0, 0);
+			while(true)
+			{
+				if(isDisposed) break;
+				
+				lock(udpSocket)
+				{
+					try
 					{
-						transactions[from] = new Dictionary<uint, Transaction>();
+						udpSocket.ReceiveFrom(packet, ref endpoint);
+					}
+					catch(SocketException e)
+					{
+						Console.WriteLine("Socket threw up a SocketException with code {0}", e.ErrorCode);
+						break;
+					}
+				}
+				
+				uint sessionId = BitConverter.ToUInt32(packet, 1);
+				PacketId id = new PacketId(sessionId, endpoint as IPEndPoint);
+				
+				if(packet[0] == DataPacket)
+				{
+					lock(answeredPackets)
+					{
+						if(answeredPackets.Contains(id) || packetsToAnswer.Contains(id)) continue;
+						answeredPackets.Add(id);
 					}
 					
-					hostTransactions = transactions[from];
-				}
-				
-				uint sessionId = BitConverter.ToUInt32(buffer, 0);
-				sessionId ^= localSessionMask;
-				byte[] packetData = buffer.Skip(sizeof(uint)).ToArray();
-				
-				Transaction incomingTransaction;
-				lock(hostTransactions)
-				{
-					if(!hostTransactions.ContainsKey(sessionId))
+					lock(readyData)
 					{
-						if((sessionId & localSessionMask) == localSessionMask)
-						{
-							throw new KeyNotFoundException(string.Format("Session id {0} should have been local but does not exist", sessionId));
-						}
-						incomingTransaction = new ReceivingTransaction(this, from);
+						readyData.Enqueue(new NetworkEventArgs(id.RemoteHost, packet.Skip(5).ToArray()));
 					}
-					else
+					
+					// copy the session id
+					for(int i = 1; i < 5; i++)
+						answer[i] = packet[i];
+					
+					lock(udpSocket)
 					{
-						incomingTransaction = hostTransactions[sessionId];
+						udpSocket.SendTo(answer, endpoint);
 					}
 				}
-				
-				lock(incomingTransaction)
+				else
 				{
-					incomingTransaction.Receive(packetData);
-					if(incomingTransaction.IsCompleted)
+					lock(packetsToSend)
 					{
-						if(incomingTransaction is ReceivingTransaction)
+						if(packetsToSend.ContainsKey(id))
 						{
-							lock(completedTransactions)
-							{
-								completedTransactions.Enqueue(incomingTransaction as ReceivingTransaction);
-							}
+							AddPing(id.RemoteHost, packetsToSend[id].Acknowledge());
+							packetsToSend.Remove(id);
 						}
-						
-						lock(hostTransactions)
+					}
+				}
+			}
+		}
+		
+		private void SenderThread()
+		{
+			while(true)
+			{
+				if(isDisposed) break;
+				
+				List<PacketSession> sessions;
+				lock(packetsToSend)
+				{
+					sessions = packetsToSend.Values.ToList();
+				}
+				
+				foreach(PacketSession session in sessions)
+				{
+					if(session.NeedsResend)
+					{
+						lock(udpSocket)
 						{
-							hostTransactions.Remove(sessionId);
+							session.SendThrough(udpSocket);
+							session.ResetSendTime(AveragePing(session.Id.RemoteHost) + StandardDeviationForPings(session.Id.RemoteHost));
 						}
 					}
 				}
 				
 				Thread.Sleep(10);
-            }
-        }
-
-        #endregion
-
-        #region Internal
-
-        /// <summary>
-        /// Returns the average time packets took to reach a host over the last 50 successful tries.
-        /// </summary>
-        /// <param name="host">The <see cref="IPEndPoint"/> representing the host</param>
-        /// <returns>A <see cref="TimeSpan"/> structure indicating the average time it takes to reach the host</returns>
-        /// <remarks>If the host was never reached before, this method returns a default value of 100 milliseconds.</remarks>
-        internal TimeSpan AverageAnswerTimeForHost(IPEndPoint host)
-        {
-            double result = 0;
-			Queue<TimeSpan> timeSpans;
-			lock(answerTimes)
-			{
-	            if (!answerTimes.ContainsKey(host))
-	                return new TimeSpan(0, 0, 0, 100);
-				timeSpans = answerTimes[host];
-			}
-			
-			lock(timeSpans)
-			{
-	            foreach (TimeSpan answerTime in timeSpans)
-	            {
-	                result += answerTime.TotalMilliseconds;
-	            }
-			}
-			
-            return new TimeSpan(0, 0, 0, 0, (int)result);
-        }
-
-        internal void PushAnswerTime(IPEndPoint host, TimeSpan duration)
-        {
-			Queue<TimeSpan> timeSpans;
-			lock(answerTimes)
-			{
-	            if (!answerTimes.ContainsKey(host))
-	            {
-	                answerTimes[host] = new Queue<TimeSpan>();
-	            }
-				timeSpans = answerTimes[host];
-			}
-
-			lock(timeSpans)
-			{
-	            timeSpans.Enqueue(duration);
-	            if (timeSpans.Count > 50)
-	            {
-	                timeSpans.Dequeue();
-	            }
-			}
-        }
-
-        #endregion
-
-        #region Public
-
-		/// <summary>
-		/// Sends data over the transporter to a specified address.
-		/// </summary>
-		/// <param name="data">
-		/// The byte array to send
-		/// </param>
-		/// <param name="to">
-		/// The destination <see cref="IPEndPoint"/>
-		/// </param>
-        public void SendTo(byte[] data, IPEndPoint to)
-        {
-            CheckIfDisposed();
-            SendingTransaction transaction = new SendingTransaction(this, to, data);
-            uint sessionId;
-            lock (sessionIdLocker)
-            {
-                sessionId = nextSessionId;
-                nextSessionId++;
-                nextSessionId &= 0x7FFFFFFF;
-            }
-
-			Dictionary<uint, Transaction> hostTransactions;
-            lock (transactions)
-            {
-                if (!transactions.ContainsKey(to))
-                {
-                    transactions[to] = new Dictionary<uint, Transaction>();
-                }
-				hostTransactions = transactions[to];
-			}
-			
-			lock(hostTransactions)
-			{
-            		hostTransactions[sessionId] = transaction;
-			}
-        }
-		
-		/// <summary>
-		/// Polls the Transporter, triggering reception events for every received packet.
-		/// </summary>
-		public void Poll()
-		{
-			CheckIfDisposed();
-			var handler = Received;
-			if(handler != null)
-			{
-				lock(completedTransactions)
-				{
-					foreach(ReceivingTransaction completedTransaction in completedTransactions)
-					{
-						handler(this, new NetworkEventArgs(completedTransaction.RemoteHost, completedTransaction.Data));
-					}
-				}
 			}
 		}
-
-		/// <summary>
-		/// Disposes of the Transporter.
-		/// </summary>
-        public void Dispose()
-        {
-            CheckIfDisposed();
-            isDisposed = true;
+		
+		public void SendTo(byte[] data, IPEndPoint remoteAddress)
+		{
+			ValidateObjectState();
+			
+			uint sid = nextSessionId;
+			nextSessionId++;
+			byte[] packet = new byte[data.Length + 4];
+			BitConverter.GetBytes(sid).CopyTo(packet, 0);
+			data.CopyTo(packet, sizeof(uint));
+			
+			lock(packetsToSend)
+			{
+				PacketId id = new PacketId(sid, remoteAddress);
+				packetsToSend[id] = new PacketSession(id, packet);
+			}
+		}
+		
+		public void Dispose()
+		{
+			isDisposed = true;
 			udpSocket.Shutdown(SocketShutdown.Both);
 			udpSocket.Close();
-        }
-
-        #endregion
-
-        #endregion
-    }
-
+		}
+		
+		#endregion
+	}
 }

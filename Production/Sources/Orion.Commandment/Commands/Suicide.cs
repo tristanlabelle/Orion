bb5ef1261a0de10111f9411c4b1f723482cd6f81
Mathieu Lavoie <>
2009-11-03 20:06:22 +0000

    Forget suicide command class

---
+++ Production/Sources/Orion.Commandment/Commands/Suicide.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Orion.GameLogic;
+
+namespace Orion.Commandment.Commands
+{
+    class Suicide : Command
+    {
+        #region Fields
+        List<Unit> suiciders;
+        #endregion
+
+        #region Constructor
+        public Suicide(Faction faction, IEnumerable<Unit> units)
+            : base(faction)
+        {
+            Argument.EnsureNoneNull(units, "units");
+            this.suiciders = units.ToList();
+        }
+        #endregion
+
+
+        #region Methods
+        public override void Execute()
+        {
+            foreach (Unit suicider in suiciders)
+            {
+                suicider.Suicide();
+            }
+        }
+        #endregion
+    }
+}

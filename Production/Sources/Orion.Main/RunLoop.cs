1974021e366da564adb813eda2900c27d9e3b5c5
Tristan Labelle <git@tristanlabelle.com>
2009-11-06 15:33:06 +0000

    First pass of run loop refactoring.

--- Production/Sources/Orion.Main/RunLoop.cs
+++ Production/Sources/Orion.Main/RunLoop.cs
@@ -5,71 +5,28 @@ using Orion.UserInterface;
 
 namespace Orion.Main
 {
-    public enum DeferenceType
-    {
-        Defer, Stack
-    }
-
     abstract class RunLoop
     {
         #region Fields
-        private RunLoop parent;
-        private RunLoop child;
-
         protected GameUI userInterface;
         #endregion
 
         #region Constructors
-
         public RunLoop(GameUI ui)
         {
             userInterface = ui;
         }
-
         #endregion
 
         #region Methods
-
-        public void DeferTo(RunLoop runLoop, DeferenceType how)
-        {
-            if (how == DeferenceType.Defer)
-            {
-                if (parent == null) throw new InvalidOperationException("Cannot fully defer execution of the main run loop");
-                parent.child = runLoop;
-            }
-            else
-            {
-                runLoop.parent = this;
-                child = runLoop;
-            }
-        }
-
         public void RunOnce()
-        {
-            PrepareRun();
-            if (child == null) RunLoopMain();
-            else child.RunOnce();
-            CleanupRun();
-        }
-
-        protected virtual void PrepareRun()
         {
             Application.DoEvents();
             userInterface.Refresh();
+            RunLoopMain();
         }
 
         protected abstract void RunLoopMain();
-
-        protected virtual void CleanupRun()
-        { }
-
-        protected void ExitRunLoop()
-        {
-            // this makes little sense
-            if (parent == null) throw new InvalidOperationException("Cannot exit the main run loop");
-            parent.child = null;
-        }
-
         #endregion
     }
 }

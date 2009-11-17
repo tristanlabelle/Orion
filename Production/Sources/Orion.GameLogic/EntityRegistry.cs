b2d8ce5250fbcac7958180c0426cd72ba088f327
Tristan Labelle <git@tristanlabelle.com>
2009-11-17 14:57:24 +0000

    Completed buffer pooling refactoring/optimization in EntityRegistry.

--- Production/Sources/Orion.GameLogic/EntityRegistry.cs
+++ Production/Sources/Orion.GameLogic/EntityRegistry.cs
@@ -19,119 +19,11 @@ namespace Orion.GameLogic
     [Serializable]
     public sealed class EntityRegistry : IEnumerable<Entity>
     {
-        #region Nested Types
-        /// <summary>
-        /// Represents a rectangular spatial subdivision used to group nearby <see cref="Entity"/>s.
-        /// </summary>
-        [DebuggerDisplay("Count = {Count}")]
-        private sealed class Zone
-        {
-            #region Fields
-            private readonly BufferPool<Entity> bufferPool;
-            private Entity[] unitBuffer;
-            private int count;
-            #endregion
-
-            #region Constructors
-            public Zone(BufferPool<Entity> bufferPool)
-            {
-                Argument.EnsureNotNull(bufferPool, "bufferPool");
-                this.bufferPool = bufferPool;
-            }
-            #endregion
-
-            #region Properties
-            /// <summary>
-            /// Gets the number of units in this zone.
-            /// </summary>
-            public int Count
-            {
-                get { return count; }
-            }
-            #endregion
-
-            #region Indexers
-            /// <summary>
-            /// Gets a <see cref="Entity"/> from this <see cref="Zone"/> by its index.
-            /// </summary>
-            /// <param name="index">The index of the <see cref="Entity"/> to be retrieved.</param>
-            /// <returns>The <see cref="Entity"/> at that index.</returns>
-            public Entity this[int index]
-            {
-                get { return unitBuffer[index]; }
-            }
-            #endregion
-
-            #region Methods
-            /// <summary>
-            /// Removes a <see cref="Entity"/> from this zone.
-            /// </summary>
-            /// <param name="item">The <see cref="Entity"/> to be removed.</param>
-            /// <returns><c>True</c> if an <see cref="Entity"/> was removed, <c>false</c> if it wasn't found.</returns>
-            public bool Remove(Entity entity)
-            {
-                int index = IndexOf(entity);
-                if (index == -1) return false;
-                RemoveAt(index);
-                return true;
-            }
-
-            /// <summary>
-            /// Gets the index of the first (and hopefully only) occurance
-            /// of an <see cref="Entity"/> in this <see cref="Zone"/>.
-            /// </summary>
-            /// <param name="entity">The <see cref="Entity"/> to be found.</param>
-            /// <returns>The index of <paramref name="entity"/>, or <c>-1</c> if it wasn't found.</returns>
-            public int IndexOf(Entity entity)
-            {
-                for (int i = 0; i < count; ++i)
-                    if (unitBuffer[i] == entity)
-                        return i;
-                return -1;
-            }
-
-            /// <summary>
-            /// Removes an <see cref="Entity"/> at an index in this <see cref="Zone"/>
-            /// </summary>
-            /// <param name="index">The index of the entity to be removed.</param>
-            public void RemoveAt(int index)
-            {
-                if (index < count - 1) unitBuffer[index] = unitBuffer[count - 1];
-                unitBuffer[count - 1] = null;
-                --count;
-            }
-
-            /// <summary>
-            /// Adds a <see cref="Entity"/> to this <see cref="Zone"/>
-            /// The <see cref="Entity"/> is assumed to be absent.
-            /// </summary>
-            /// <param name="item">The <see cref="Entity"/> to be added.</param>
-            public void Add(Entity entity)
-            {
-                if (unitBuffer == null)
-                {
-                    unitBuffer = bufferPool.Get(1);
-                }
-                else if (count == unitBuffer.Length)
-                {
-                    Entity[] newUnitBuffer = bufferPool.Get(unitBuffer.Length + 1);
-                    Array.Copy(unitBuffer, newUnitBuffer, unitBuffer.Length);
-                    bufferPool.Add(unitBuffer);
-                    unitBuffer = newUnitBuffer;
-                }
-
-                unitBuffer[count] = entity;
-                ++count;
-            }
-            #endregion
-        }
-        #endregion
-
         #region Instance
         #region Fields
         private readonly World world;
         private readonly List<Entity> entities = new List<Entity>();
-        private readonly Zone[,] zones;
+        private readonly EntityZone[,] zones;
         private readonly BufferPool<Entity> bufferPool = CreateBufferPool();
 
         // Temporary collections used to defer modification of "entities"
@@ -214,18 +106,18 @@ namespace Orion.GameLogic
 
         private int UnitsInZones
         {
-            get { return zones.Cast<Zone>().Sum(zone => zone.Count); }
+            get { return zones.Cast<EntityZone>().Sum(zone => zone.Count); }
         }
         #endregion
 
         #region Methods
         #region Initialization
-        private static Zone[,] CreateZones(int columnCount, int rowCount, BufferPool<Entity> bufferPool)
+        private static EntityZone[,] CreateZones(int columnCount, int rowCount, BufferPool<Entity> bufferPool)
         {
-            Zone[,] zones = new Zone[columnCount, rowCount];
+            EntityZone[,] zones = new EntityZone[columnCount, rowCount];
             for (int columnIndex = 0; columnIndex < columnCount; ++columnIndex)
                 for (int rowIndex = 0; rowIndex < rowCount; ++rowIndex)
-                    zones[columnIndex, rowIndex] = new Zone(bufferPool);
+                    zones[columnIndex, rowIndex] = new EntityZone(bufferPool);
             return zones;
         }
 
@@ -447,7 +339,7 @@ namespace Orion.GameLogic
             {
                 for (int y = minZoneCoords.Y; y <= maxZoneCoords.Y; ++y)
                 {
-                    Zone zone = zones[x, y];
+                    EntityZone zone = zones[x, y];
                     for (int i = 0; i < zone.Count; ++i)
                     {
                         Entity entity = zone[i];
@@ -477,7 +369,7 @@ namespace Orion.GameLogic
             {
                 for (int y = minZoneCoords.Y; y <= maxZoneCoords.Y; ++y)
                 {
-                    Zone zone = zones[x, y];
+                    EntityZone zone = zones[x, y];
                     for (int i = 0; i < zone.Count; ++i)
                     {
                         Entity entity = zone[i];

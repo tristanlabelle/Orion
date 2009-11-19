8f1163b3920ba81b53f177b652510f8c8864b8f5
Jcee <>
2009-11-19 23:44:04 +0000

    Started To work on Frame, code commented.

--- Production/Sources/Orion.Graphics/Renderers/UnitFrameRenderer.cs
+++ Production/Sources/Orion.Graphics/Renderers/UnitFrameRenderer.cs
@@ -7,6 +7,7 @@ using System.Drawing;
 using OpenTK.Math;
 using Orion.GameLogic;
 
+
 namespace Orion.Graphics
 {
     public class UnitFrameRenderer : FrameRenderer
@@ -20,6 +21,18 @@ namespace Orion.Graphics
 
         public override void Draw(GraphicsContext context)
         {
+            // If a factory is selected and that it is currently executing a task. 
+            // if (unit.Type.Name=="Factory" && unit.Task!=null)
+            //{
+             /* Orion.Geometry.Rectangle rect = new Orion.Geometry.Rectangle(150,10,60,100);
+                context.FillColor = Color.Black; 
+                context.Fill(rect);
+                context.FillColor = Color.White;
+                context.Stroke(rect);
+                base.Draw(context); 
+            // context.Draw(
+             */  
+            // }
             string hp = "HP: {0}/{1}".FormatInvariant(unit.Health, unit.MaxHealth);
             context.FillColor = Color.DarkBlue;
             context.Draw(unit.Type.Name, new Vector2(150, 155));

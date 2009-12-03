805d824c5e1c6ca99686731613ef3ec4a2fb0fbe
Félix Cloutier <>
2009-12-03 20:44:37 +0000

    Fixed the temporal paradoxes in multiplayer games (and, as an incidental effect, made the game run smoother).

--- Production/Sources/Orion.Networking/CommandSynchronizer.cs
+++ Production/Sources/Orion.Networking/CommandSynchronizer.cs
@@ -97,7 +97,7 @@ namespace Orion.Networking
                 peers.ForEach(peer => peer.SendDone(commandFrameNumber, updatesSinceLastCommandFrame));
             }
 
-            if (updatesSinceLastCommandFrame > TargetUpdatesPerCommandFrame)
+            if (updatesSinceLastCommandFrame >= TargetUpdatesPerCommandFrame * 2)
             {
                 if (!AllPeersDone || !ReceivedFromAllPeers)
                 {

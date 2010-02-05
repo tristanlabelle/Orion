66b30ef11e09ab0811eab08f6557ce7af69fc5fa
Tristan Labelle <git@tristanlabelle.com>
2010-02-05 03:15:17 +0000

    Created and started implementation of Orion.Audio.

---
+++ Production/Sources/Orion.Audio/AudioContext.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenTK;
+using OpenTK.Audio;
+using System.IO;
+using System.Diagnostics;
+
+namespace Orion.Audio
+{
+    public sealed class AudioContext : IDisposable
+    {
+        #region Fields
+        private readonly IntPtr deviceHandle;
+        private readonly ContextHandle contextHandle;
+        #endregion
+
+        #region Constructors
+        public AudioContext(string directory)
+        {
+            Argument.EnsureNotNull(directory, "directory");
+
+            deviceHandle = Alc.OpenDevice(null);
+            if (deviceHandle == null) return;
+
+            contextHandle = Alc.CreateContext(deviceHandle, (int[])null);
+            if (contextHandle == null) return;
+
+            Alc.MakeContextCurrent(contextHandle);
+            Debug.Assert(AL.GetError() == ALError.NoError);
+
+            Alut.InitWithoutContext();
+            Debug.Assert(AL.GetError() == ALError.NoError);
+
+            foreach (string file in Directory.GetFiles(directory, "*.wav"))
+            {
+                Alut.CreateBufferFromFile(
+            }
+        }
+        #endregion
+
+        #region Properties
+        public string DeviceName
+        {
+            get
+            {
+                return contextHandle.Handle == IntPtr.Zero
+                    ? null
+                    : Alc.GetString(deviceHandle, AlcGetString.DefaultDeviceSpecifier);
+            }
+        }
+
+        public bool IsDummy
+        {
+            get { return contextHandle.Handle == IntPtr.Zero; }
+        }
+        #endregion
+
+        #region Methods
+        public void Dispose()
+        {
+            if (contextHandle != null) Alc.DestroyContext(contextHandle);
+            if (deviceHandle != null) Alc.CloseDevice(deviceHandle);
+        }
+        #endregion
+    }
+}
---
+++ Production/Sources/Orion.Audio/SoundBuffer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using OpenTK.Audio;
+using System.Diagnostics;
+
+namespace Orion.Audio
+{
+    internal sealed class SoundBuffer : IDisposable
+    {
+        #region Fields
+        private readonly uint handle;
+        private readonly string name;
+        #endregion
+
+        #region Constructors
+        public SoundBuffer(string filePath)
+        {
+            Argument.EnsureNotNull(filePath, "filePath");
+
+            handle = Alut.CreateBufferFromFile(filePath);
+            Debug.Assert(handle != 0);
+            name = Path.GetFileNameWithoutExtension(filePath);
+        }
+        #endregion
+
+        #region Properties
+        public uint Handle
+        {
+            get { return handle; }
+        }
+        #endregion
+
+        #region Methods
+        public void Dispose()
+        {
+            uint handle = this.handle;
+            AL.DeleteBuffer(ref handle);
+            Debug.Assert(AL.GetError() == ALError.NoError);
+        }
+        #endregion
+    }
+}

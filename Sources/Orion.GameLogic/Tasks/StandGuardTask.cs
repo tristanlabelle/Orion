2c5165fe54f7d5318aec9abcadb7569bffa6fe25
Tristan Labelle <git@tristanlabelle.com>
2010-02-17 15:07:12 +0000

    Optimized stand guard task by distributing enemy checks on multiple frames.

--- Sources/Orion.GameLogic/Tasks/StandGuardTask.cs
+++ Sources/Orion.GameLogic/Tasks/StandGuardTask.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using Orion.Geometry;
 using OpenTK.Math;
+using Orion.GameLogic.Skills;
 
 namespace Orion.GameLogic.Tasks
 {
@@ -12,26 +13,25 @@ namespace Orion.GameLogic.Tasks
     public sealed class StandGuardTask : Task
     {
         #region Fields
+        /// <summary>
+        /// The number of frames between checks for surrounding enemies.
+        /// Used to distribute enemy checks on multiple frames so its not too heavy.
+        /// </summary>
+        private static int enemyCheckInterval = 6;
 
         private Unit target;
-
         #endregion
 
         #region Constructors
-
         public StandGuardTask(Unit guard)
             : base(guard)
-        {
-            Argument.EnsureNotNull(guard, "guard");
-        }
-
+        {}
         #endregion
 
         #region Properties
-
-        public Unit Guard
+        public Unit Target
         {
-            get { return Unit; }
+            get { return target; }
         }
 
         public override string Description
@@ -43,36 +43,40 @@ namespace Orion.GameLogic.Tasks
         {
             get { return false; }
         }
-
         #endregion
 
         #region Methods
-
         protected override void DoUpdate(SimulationStep step)
         {
-            if (Unit.HasSkill<Skills.AttackSkill>())
+            if (!Unit.HasSkill<AttackSkill>()) return;
+
+            if (!IsTargetValid(target))
             {
-                if (!IsTargetValid(target))
+                bool canAttemptFindingTarget = ((step.Number + Unit.Handle.Value) % enemyCheckInterval) == 0;
+                if (canAttemptFindingTarget)
+                {
                     target = Unit.World.Entities
                         .Intersecting(Unit.LineOfSight)
                         .OfType<Unit>()
                         .FirstOrDefault(other => Unit.IsWithinAttackRange(other)
                             && Unit.Faction.GetDiplomaticStance(other.Faction) == DiplomaticStance.Enemy);
-                if (target != null)
-                {
-                    Unit.LookAt(target.Center);
-                    Unit.TryHit(target);
                 }
+                else
+                {
+                    target = null;
+                }
+            }
+
+            if (target != null)
+            {
+                Unit.LookAt(target.Center);
+                Unit.TryHit(target);
             }
         }
 
         private bool IsTargetValid(Unit target)
         {
-            if (target != null)
-                if (target.IsAlive)
-                    if (Unit.IsWithinAttackRange(target))
-                        return true;
-            return false;
+            return target != null && target.IsAlive && Unit.IsWithinAttackRange(target);
         }
         #endregion
     }

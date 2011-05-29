7a4ce6d78aa5c78cef69329de0ceafab556d14e2
Tristan Labelle <git@tristanlabelle.com>
2011-05-29 19:10:38 +0000

    Added fixed-point number and vector2 classes, though they are not used yet.

---
+++ Sources/Engine/Fixed.cs
@@ -0,0 +1,514 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+using System.ComponentModel;
+
+namespace Orion.Engine
+{
+    /// <summary>
+    /// Represents 32 bits fixed-point numbers in Q16.16 format.
+    /// </summary>
+    [Serializable]
+    [StructLayout(LayoutKind.Sequential, Size = 4)]
+    [ImmutableObject(true)]
+    public struct Fixed : IEquatable<Fixed>, IComparable, IComparable<Fixed>, IFormattable
+    {
+        #region Instance
+        #region Fields
+        private readonly int rawValue;
+        #endregion
+
+        #region Constructors
+        private Fixed(int rawValue)
+        {
+            this.rawValue = rawValue;
+        }
+        #endregion
+
+        #region Properties
+        /// <summary>
+        /// Gets the internal representation of the fixed-point number.
+        /// </summary>
+        public int RawValue
+        {
+            get { return rawValue; }
+        }
+        #endregion
+
+        #region Methods
+        #region Conversions
+        public int ToInt32()
+        {
+            return rawValue >> FractionalBitCount;
+        }
+
+        public long ToInt64()
+        {
+            return rawValue >> FractionalBitCount;
+        }
+
+        public float ToSingle()
+        {
+            return rawValue / (float)One.rawValue;
+        }
+
+        public double ToDouble()
+        {
+            return rawValue / (double)One.rawValue;
+        }
+        #endregion
+
+        #region Comparison/Equality
+        public int CompareTo(Fixed other)
+        {
+            return rawValue.CompareTo(other.rawValue);
+        }
+
+        public bool Equals(Fixed other)
+        {
+            return rawValue == other.rawValue;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Fixed && Equals((Fixed)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return rawValue.GetHashCode();
+        }
+        #endregion
+
+        #region ToString
+        public string ToString(string format, IFormatProvider formatProvider)
+        {
+            return ToDouble().ToString(format, formatProvider);
+        }
+
+        public string ToString(string format)
+        {
+            return ToString(format, null);
+        }
+        
+        public string ToString(IFormatProvider formatProvider)
+        {
+            return ToString(null, formatProvider);
+        }
+
+        public override string ToString()
+        {
+            return ToString(null, null);
+        }
+        #endregion
+        #endregion
+
+        #region Explicit Members
+        int IComparable.CompareTo(object obj)
+        {
+ 	        if (!(obj is Fixed)) throw new ArgumentException("Cannot compare with non-Fixed object.", "obj");
+            return CompareTo((Fixed)obj);
+        }
+        #endregion
+        #endregion
+
+        #region Static
+        #region Fields
+        /// <summary>
+        /// The number of bits representing the integral part of the number.
+        /// </summary>
+        public const int IntegralBitCount = 16;
+
+        /// <summary>
+        /// The number of bits representing the fractional part of the number.
+        /// </summary>
+        public const int FractionalBitCount = 16;
+
+        /// <summary>
+        /// The bit mask for the fractional part of the raw value.
+        /// </summary>
+        public const int FractionalBitMask = (1 << FractionalBitCount) - 1;
+
+        /// <summary>
+        /// The bit mask for the integral part of the raw value.
+        /// </summary>
+        public const int IntegralBitMask = ~FractionalBitMask;
+
+        /// <summary>
+        /// A fixed-point number with a value of zero.
+        /// </summary>
+        public static readonly Fixed Zero = FromRawValue(0);
+
+        /// <summary>
+        /// A fixed-point number with a value of one.
+        /// </summary>
+        public static readonly Fixed One = FromRawValue(1 << FractionalBitCount);
+
+        /// <summary>
+        /// A fixed-point number with a value of a half.
+        /// </summary>
+        public static readonly Fixed Half = FromRawValue(1 << (FractionalBitCount - 1));
+
+        /// <summary>
+        /// A fixed-point number with the smallest fractional value.
+        /// </summary>
+        public static readonly Fixed Epsilon = FromRawValue(1);
+
+        /// <summary>
+        /// A fixed-point number with the greatest representable value.
+        /// </summary>
+        public static readonly Fixed MaxValue = FromRawValue(int.MaxValue);
+
+        /// <summary>
+        /// A fixed-point number with the lowest representable value.
+        /// </summary>
+        public static readonly Fixed MinValue = FromRawValue(int.MinValue);
+
+        /// <summary>
+        /// A fixed-point number with a value of PI.
+        /// </summary>
+        public static readonly Fixed PI = FromRawValue(205887);
+
+        /// <summary>
+        /// A fixed-point number with a value of E.
+        /// </summary>
+        public static readonly Fixed E = FromRawValue(178145);
+        #endregion
+
+        #region Methods
+        #region Factory Methods
+        public static Fixed FromRawValue(int value)
+        {
+            return FromRawValue(value);
+        }
+
+        public static Fixed FromInt32(int value)
+        {
+            return FromRawValue(value << FractionalBitCount);
+        }
+
+        public static Fixed FromInt64(long value)
+        {
+            return FromInt32((int)value);
+        }
+
+        public static Fixed FromSingle(float value)
+        {
+            return FromRawValue((int)(value * One.rawValue));
+        }
+
+        public static Fixed FromDouble(double value)
+        {
+            return FromRawValue((int)(value * One.rawValue));
+        }
+        #endregion
+
+        #region Arithmetic
+        #region Additive
+        public static Fixed Negate(Fixed value)
+        {
+            return FromRawValue(-value.rawValue);
+        }
+
+        public static Fixed Add(Fixed term1, Fixed term2)
+        {
+            return FromRawValue(term1.rawValue + term2.rawValue);
+        }
+
+        public static Fixed Subtract(Fixed term1, Fixed term2)
+        {
+            return FromRawValue(term1.rawValue - term2.rawValue);
+        }
+        #endregion
+
+        #region Multiplicative
+        public static Fixed Multiply(Fixed term1, Fixed term2)
+        {
+            long scaledProduct = (long)term1.rawValue * (long)term2.rawValue;
+            return FromRawValue((int)(scaledProduct >> FractionalBitCount));
+        }
+
+        public static Fixed Divide(Fixed term1, Fixed term2)
+        {
+            long result = ((long)term1.rawValue << FractionalBitCount) / term2.rawValue;
+            return FromRawValue((int)result);
+        }
+
+        public static Fixed Multiply(Fixed term1, int term2)
+        {
+            return FromRawValue(term1.rawValue * term2);
+        }
+
+        public static Fixed Divide(Fixed term1, int term2)
+        {
+            return FromRawValue(term1.rawValue / term2);
+        }
+
+        public static Fixed Mod(Fixed term1, Fixed term2)
+        {
+            return FromRawValue(term1.rawValue % term2.rawValue);
+        }
+        #endregion
+
+        #region Binary
+        public static Fixed LeftShift(Fixed term, int shift)
+        {
+            return FromRawValue(term.rawValue << shift);
+        }
+
+        public static Fixed RightShift(Fixed term, int shift)
+        {
+            return FromRawValue(term.rawValue >> shift);
+        }
+        #endregion
+        #endregion
+
+        #region Math Utility
+        public static int Sign(Fixed value)
+        {
+            return Math.Sign(value.rawValue);
+        }
+
+#warning Fixed-point operations should not depend on System.Math floating-point operations.
+        public static Fixed Abs(Fixed value)
+        {
+            return FromRawValue(Math.Abs(value.rawValue));
+        }
+
+        public static Fixed Sqrt(Fixed value)
+        {
+            return (Fixed)Math.Sqrt((double)value);
+        }
+
+        public static Fixed Sin(Fixed value)
+        {
+            return (Fixed)Math.Sin((double)value);
+        }
+
+        public static Fixed Cos(Fixed value)
+        {
+            return (Fixed)Math.Cos((double)value);
+        }
+
+        public static Fixed Tan(Fixed value)
+        {
+            return (Fixed)Math.Tan((double)value);
+        }
+
+        public static Fixed Atan2(Fixed y, Fixed x)
+        {
+            return (Fixed)Math.Atan2((double)y, (double)x);
+        }
+
+        public static Fixed Min(Fixed term1, Fixed term2)
+        {
+            return FromRawValue(Math.Min(term1.rawValue, term2.rawValue));
+        }
+
+        public static Fixed Max(Fixed term1, Fixed term2)
+        {
+            return FromRawValue(Math.Max(term1.rawValue, term2.rawValue));
+        }
+
+        public static Fixed Clamp(Fixed value, Fixed min, Fixed max)
+        {
+            if (value < min) return min;
+            if (value > max) return max;
+            return value;
+        }
+
+        public static Fixed Lerp(Fixed a, Fixed b, Fixed blend)
+        {
+            return a + (b - a) * blend;
+        }
+
+        public static Fixed Truncate(Fixed term)
+        {
+            if (term.rawValue >= 0)
+            {
+                return FromRawValue(term.rawValue & IntegralBitMask);
+            }
+            else
+            {
+                return FromRawValue((int)-((-(long)term.rawValue) & IntegralBitMask));
+            }
+        }
+
+        public static Fixed Round(Fixed term)
+        {
+            if (term.rawValue >= 0)
+            {
+                return FromRawValue((term.rawValue + Half.rawValue) & IntegralBitMask);
+            }
+            else
+            {
+                return FromRawValue((int)-((-(long)term.rawValue + Half.rawValue) & IntegralBitMask));
+            }
+        }
+        #endregion
+
+        #region Object Model
+        public static bool Equals(Fixed term1, Fixed term2)
+        {
+            return term1.Equals(term2);
+        }
+
+        public static int Compare(Fixed term1, Fixed term2)
+        {
+            return term1.CompareTo(term2);
+        }
+        #endregion
+        #endregion
+
+        #region Operators
+        #region Arithmetic
+        #region Additive
+        public static Fixed operator +(Fixed term)
+        {
+            return term;
+        }
+
+        public static Fixed operator -(Fixed term)
+        {
+            return Negate(term);
+        }
+
+        public static Fixed operator +(Fixed lhs, Fixed rhs)
+        {
+            return Add(lhs, rhs);
+        }
+
+        public static Fixed operator -(Fixed lhs, Fixed rhs)
+        {
+            return Subtract(lhs, rhs);
+        }
+        #endregion
+
+        #region Multiplicative
+        public static Fixed operator *(Fixed lhs, Fixed rhs)
+        {
+            return Multiply(lhs, rhs);
+        }
+
+        public static Fixed operator /(Fixed lhs, Fixed rhs)
+        {
+            return Divide(lhs, rhs);
+        }
+
+        public static Fixed operator *(Fixed lhs, int rhs)
+        {
+            return Multiply(lhs, rhs);
+        }
+
+        public static Fixed operator /(Fixed lhs, int rhs)
+        {
+            return Divide(lhs, rhs);
+        }
+
+        public static Fixed operator *(int lhs, Fixed rhs)
+        {
+            return Multiply(rhs, lhs);
+        }
+
+        public static Fixed operator %(Fixed lhs, Fixed rhs)
+        {
+            return Mod(lhs, rhs);
+        }
+        #endregion
+
+        #region Binary
+        public static Fixed operator <<(Fixed lhs, int rhs)
+        {
+            return LeftShift(lhs, rhs);
+        }
+
+        public static Fixed operator >>(Fixed lhs, int rhs)
+        {
+            return RightShift(lhs, rhs);
+        }
+        #endregion
+        #endregion
+
+        #region Equality
+        public static bool operator ==(Fixed lhs, Fixed rhs)
+        {
+            return Equals(lhs, rhs);
+        }
+
+        public static bool operator !=(Fixed lhs, Fixed rhs)
+        {
+            return !Equals(lhs, rhs);
+        }
+        #endregion
+
+        #region Comparison
+        public static bool operator <(Fixed lhs, Fixed rhs)
+        {
+            return Compare(lhs, rhs) < 0;
+        }
+
+        public static bool operator <=(Fixed lhs, Fixed rhs)
+        {
+            return Compare(lhs, rhs) <= 0;
+        }
+
+        public static bool operator >(Fixed lhs, Fixed rhs)
+        {
+            return Compare(lhs, rhs) > 0;
+        }
+
+        public static bool operator >=(Fixed lhs, Fixed rhs)
+        {
+            return Compare(lhs, rhs) >= 0;
+        }
+        #endregion
+
+        #region Casting To Other Types
+        public static explicit operator int(Fixed value)
+        {
+            return value.ToInt32();
+        }
+
+        public static explicit operator long(Fixed value)
+        {
+            return value.ToInt64();
+        }
+
+        public static explicit operator float(Fixed value)
+        {
+            return value.ToSingle();
+        }
+
+        public static explicit operator double(Fixed value)
+        {
+            return value.ToDouble();
+        }
+        #endregion
+
+        #region Casting From Other Types
+        public static implicit operator Fixed(int value)
+        {
+            return FromInt32(value);
+        }
+
+        public static explicit operator Fixed(long value)
+        {
+            return FromInt64(value);
+        }
+
+        public static explicit operator Fixed(float value)
+        {
+            return FromSingle(value);
+        }
+
+        public static explicit operator Fixed(double value)
+        {
+            return FromDouble(value);
+        }
+        #endregion
+        #endregion
+        #endregion
+    }
+}
---
+++ Sources/Engine/Fixed2.cs
@@ -0,0 +1,259 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace Orion.Engine
+{
+    /// <summary>
+    /// A 2D fixed-point vector.
+    /// </summary>
+    [Serializable]
+    [StructLayout(LayoutKind.Sequential, Size = 8)]
+    public struct Fixed2 : IEquatable<Fixed2>
+    {
+        #region Instance
+        #region Fields
+        public Fixed X;
+        public Fixed Y;
+        #endregion
+
+        #region Constructors
+        /// <summary>
+        /// Initializes a new vector from its X and Y components.
+        /// </summary>
+        /// <param name="x">The X component of the vector.</param>
+        /// <param name="y">The Y component of the vector.</param>
+        public Fixed2(Fixed x, Fixed y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+        #endregion
+
+        #region Properties
+        /// <summary>
+        /// Gets the squared length of this vector.
+        /// </summary>
+        public Fixed SquaredLength
+        {
+            get
+            {
+                long scaledResult = (long)X.RawValue * (long)X.RawValue
+                    + (long)Y.RawValue * (long)Y.RawValue;
+                return (int)(scaledResult >> (Fixed.FractionalBitCount * 2));
+            }
+        }
+
+        /// <summary>
+        /// Gets the length of this vector.
+        /// </summary>
+        public Fixed Length
+        {
+            get { return Fixed.Sqrt(SquaredLength); }
+        }
+        #endregion
+
+        #region Methods
+        public bool Equals(Fixed2 other)
+        {
+            return X == other.X && Y == other.Y;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Fixed2 && Equals((Fixed2)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return ((X.RawValue << 16) ^ (X.RawValue >> 16)) ^ Y.RawValue;
+        }
+
+        public override string ToString()
+        {
+            return new StringBuilder()
+                .Append('[')
+                .Append(X)
+                .Append(' ')
+                .Append(Y)
+                .Append(']')
+                .ToString();
+        }
+        #endregion
+        #endregion
+
+        #region Static
+        #region Fields
+        /// <summary>
+        /// A vector with a value of zero.
+        /// </summary>
+        public static readonly Fixed2 Zero = new Fixed2(0, 0);
+
+        /// <summary>
+        /// A vector with both components set to one.
+        /// </summary>
+        public static readonly Fixed2 One = new Fixed2(1, 1);
+
+        /// <summary>
+        /// A vector with a value of one along the X axis.
+        /// </summary>
+        public static readonly Fixed2 UnitX = new Fixed2(1, 0);
+
+        /// <summary>
+        /// A vector with a value of one along the Y axis.
+        /// </summary>
+        public static readonly Fixed2 UnitY = new Fixed2(0, 1);
+        #endregion
+
+        #region Methods
+        public static bool Equals(Fixed2 first, Fixed2 second)
+        {
+            return first.Equals(second);
+        }
+
+        #region Additive
+        public static Fixed2 Negate(Fixed2 value)
+        {
+            return new Fixed2(-value.X, -value.Y);
+        }
+
+        public static Fixed2 Add(Fixed2 first, Fixed2 second)
+        {
+            return new Fixed2(first.X + second.X, first.Y + second.Y);
+        }
+
+        public static Fixed2 Subtract(Fixed2 first, Fixed2 second)
+        {
+            return new Fixed2(first.X - second.X, first.Y - second.Y);
+        }
+        #endregion
+
+        #region Multiplicative
+        public static Fixed2 Multiply(Fixed2 vector, Fixed scalar)
+        {
+            return new Fixed2(vector.X * scalar, vector.Y * scalar);
+        }
+
+        public static Fixed2 Multiply(Fixed2 vector, int scalar)
+        {
+            return new Fixed2(vector.X * scalar, vector.Y * scalar);
+        }
+
+        public static Fixed2 Divide(Fixed2 vector, Fixed scalar)
+        {
+            return new Fixed2(vector.X / scalar, vector.Y / scalar);
+        }
+
+        public static Fixed2 Divide(Fixed2 vector, int scalar)
+        {
+            return new Fixed2(vector.X / scalar, vector.Y / scalar);
+        }
+        #endregion
+
+        public static Fixed Dot(Fixed2 first, Fixed2 second)
+        {
+            return first.X * second.X + first.Y * second.Y;
+        }
+
+        #region Normalization
+        public static Fixed2 Normalize(Fixed2 vector)
+        {
+            Fixed length = vector.Length;
+            return length == 0 ? Fixed2.Zero : vector / length;
+        }
+
+        public static Fixed2 Normalize(Fixed2 vector, out Fixed length)
+        {
+            length = vector.Length;
+            return length == 0 ? Fixed2.Zero : vector / length;
+        }
+        #endregion
+
+        #region Utility
+        public static Fixed2 Min(Fixed2 first, Fixed2 second)
+        {
+            return new Fixed2(Fixed.Min(first.X, second.X), Fixed.Min(first.Y, second.Y));
+        }
+        
+        public static Fixed2 Max(Fixed2 first, Fixed2 second)
+        {
+            return new Fixed2(Fixed.Max(first.X, second.X), Fixed.Max(first.Y, second.Y));
+        }
+
+        public static Fixed2 Clamp(Fixed2 value, Fixed2 min, Fixed2 max)
+        {
+            return new Fixed2(Fixed.Clamp(value.X, min.X, max.X), Fixed.Clamp(value.Y, min.Y, max.Y));
+        }
+
+        public static Fixed2 Lerp(Fixed2 a, Fixed2 b, Fixed blend)
+        {
+            return a + (b - a) * blend;
+        }
+        #endregion
+        #endregion
+
+        #region Operators
+        #region Additive
+        public static Fixed2 operator -(Fixed2 value)
+        {
+            return Negate(value);
+        }
+
+        public static Fixed2 operator +(Fixed2 lhs, Fixed2 rhs)
+        {
+            return Add(lhs, rhs);
+        }
+
+        public static Fixed2 operator -(Fixed2 lhs, Fixed2 rhs)
+        {
+            return Subtract(lhs, rhs);
+        }
+        #endregion
+
+        #region Multiplicative
+        public static Fixed2 operator *(Fixed2 lhs, Fixed rhs)
+        {
+            return Multiply(lhs, rhs);
+        }
+
+        public static Fixed2 operator *(Fixed2 lhs, int rhs)
+        {
+            return Multiply(lhs, rhs);
+        }
+
+        public static Fixed2 operator *(int lhs, Fixed2 rhs)
+        {
+            return Multiply(rhs, lhs);
+        }
+
+        public static Fixed2 operator /(Fixed2 lhs, Fixed rhs)
+        {
+            return Divide(lhs, rhs);
+        }
+
+        public static Fixed2 operator /(Fixed2 lhs, int rhs)
+        {
+            return Divide(lhs, rhs);
+        }
+        #endregion
+
+        public static Fixed operator *(Fixed2 lhs, Fixed2 rhs)
+        {
+            return Dot(lhs, rhs);
+        }
+
+        public static bool operator ==(Fixed2 lhs, Fixed2 rhs)
+        {
+            return Equals(lhs, rhs);
+        }
+
+        public static bool operator !=(Fixed2 lhs, Fixed2 rhs)
+        {
+            return !Equals(lhs, rhs);
+        }
+        #endregion
+        #endregion
+    }
+}
